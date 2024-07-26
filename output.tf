40f9f944b933cca6042917c52dde194511d93790
Renat Gofman <renatgofman@gmail.com>
2024-07-26 14:30:09 +0300

    Add custom domain for cognito api

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 # outputs.tf
 output "invoke_urls" {
-  value = { for id,value in var.user_node_ids : id => "${aws_api_gateway_deployment.deployment.invoke_url}/nodes/${var.user_id}/${id}/" }
+  value = { for id,value in var.user_node_ids : id => "https://cognito-node-api.thunderstack.org/nodes/${var.user_id}/${id}/" }
   description = "The invoke URLs for each node ID."
 }
