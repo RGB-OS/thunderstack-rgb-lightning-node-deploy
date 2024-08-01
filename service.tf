f1ebeaeed0be706c8195746854738a5a43d33e4d
Renat Gofman <renatgofman@gmail.com>
2024-08-01 15:20:23 +0300

    Add ECS Service Discovery

--- service.tf
+++ service.tf
@@ -1,4 +1,15 @@
 # Create ecs service
+# Data source to refer to the existing Service Discovery namespace
+data "aws_service_discovery_namespace" "existing_namespace" {
+  name = "peers.thunderstack.org"
+}
+
+# Data source to refer to the existing Service Discovery service
+data "aws_service_discovery_service" "existing_service" {
+  name         = "lightning"
+  namespace_id = data.aws_service_discovery_namespace.existing_namespace.id
+}
+
 resource "aws_ecs_service" "rgb_service" {
   for_each = var.user_node_ids
   enable_execute_command  = true
@@ -11,6 +22,12 @@ resource "aws_ecs_service" "rgb_service" {
     target_group_arn = aws_lb_target_group.target_group[each.key].arn # Reference the target group
     container_name   = aws_ecs_task_definition.rgb_task[each.key].family
     container_port   = each.value
+  }
+
+  service_registries {
+    registry_arn    = data.aws_service_discovery_service.existing_service.arn
+    container_name  = aws_ecs_task_definition.rgb_task[each.key].family
+    container_port  = min(65535, 9000 + tonumber(each.value))
   }
   
   tags = {
