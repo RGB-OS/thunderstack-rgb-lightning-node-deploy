ba8c7f2d73ff34e1f5a516c9eeb68faf69bd7c1c
Renat Gofman <renatgofman@gmail.com>
2024-08-19 15:19:32 +0300

    change structure

--- deployment/deployment.tf
+++ deployment/deployment.tf
@@ -1,9 +1,31 @@
-resource "aws_api_gateway_deployment" "deployment" {
-  rest_api_id = var.rest_api_id
+resource "aws_api_gateway_deployment" "deployment_1" {
+  rest_api_id = "8619bu4cli"
   stage_name  = "dev"
-
+  
   description = "Deployment at ${timestamp()}"
+  
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
+resource "aws_api_gateway_deployment" "deployment_2" {
+  rest_api_id = "47c4q0dr04"
+  stage_name  = "dev"
+  
+  description = "Deployment at ${timestamp()}"
+  
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_deployment" "deployment_3" {
+  rest_api_id = "nvuiiz6k23"
+  stage_name  = "dev"
+  
+  description = "Deployment at ${timestamp()}"
+  
   lifecycle {
     create_before_destroy = true
   }
