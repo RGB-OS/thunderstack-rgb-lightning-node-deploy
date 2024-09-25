1711f9f48424e6e67a1563dcca0a9c77dcc977a5
Renat Gofman <renatgofman@gmail.com>
2024-09-25 15:10:19 +0300

    Change ECS service name to node-id

--- main/service.tf
+++ main/service.tf
@@ -23,7 +23,7 @@ resource "aws_service_discovery_service" "rgb_service_discovery" {
 resource "aws_ecs_service" "rgb_service" {
   for_each = var.user_node_ids
   enable_execute_command  = true
-  name            = "lightning-${var.user_id}-${each.key}"     # Name the service
+  name            = "${each.key}"     # Name the service
   cluster         = "arn:aws:ecs:us-east-2:339712759892:cluster/default"   # Reference the created Cluster
   task_definition = "${aws_ecs_task_definition.rgb_task[each.key].arn}" # Reference the task that the service will spin up
   launch_type     = "EC2"
