5d487eb0d520e7389801139db27adcc5d78a89dc
Renat Gofman <renatgofman@gmail.com>
2024-10-30 16:41:20 +0200

    Add to output

--- main/output.tf
+++ main/output.tf
@@ -3,3 +3,18 @@ output "invoke_urls" {
   value = { for id,value in var.user_node_ids : id => "https://cognito-node-api.thunderstack.org/nodes/${var.user_id}/${id}/" }
   description = "The invoke URLs for each node ID."
 }
+
+output "network_load_balancer_arn" {
+  description = "The ARN of the Network Load Balancer"
+  value       = var.network_load_balancer_arn
+}
+
+output "network_load_balancer_dns" {
+  description = "The DNS name of the Network Load Balancer"
+  value       = var.network_load_balancer_dns
+}
+
+output "api_gateway_vpclink_id" {
+  description = "The API Gateway VPC Link ID"
+  value       = var.api_gateway_vpclink_id
+}
